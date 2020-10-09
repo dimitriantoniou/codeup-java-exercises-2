0d410f1deff757de1af7fe2bfbf27e00070acaa3
Dimitri <dma2012@gmail.com>
2020-10-09 07:11:06 -0400

    Syntax, Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,40 @@
 public class HelloWorld {
     public static void main (String [] args){
         System.out.println("Hello,World!");
+        int myFavoriteNumber = 22;
+        System.out.println("My favorite number is "+myFavoriteNumber);
+        String myString = "Dimitri";
+        System.out.println("My name is "+myString);
+        int myNumber=123;
+        System.out.println("My number is "+myNumber);
+
+        /*
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+         */
+        /*
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+         */
+/*
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        int three = (int) "three";
+ */
+        int x = 4;
+        x += 5;
+
+        int a=3;
+        int b=4;
+        b*=a;
+
+        int j=10;
+        int k=2;
+        j/=k;
+        k-=j;
     }
 }
