0913f91c93b4455beb61febbb2487371892dc0d3
Dimitri <dma2012@gmail.com>
2020-10-10 09:39:06 -0400

    import scanner and declared userInt variable

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args){
         double pi=3.14159;
         System.out.format("The value of pi is approximately %.2f%n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter an integer. ");
+        int userInt=scanner.nextInt();
+        System.out.print("You entered: "+userInt);
     }
 }
