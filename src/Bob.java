ba056954ce5735b9cb3026ca0f22e3093b847e74
Dimitri <dma2012@gmail.com>
2020-10-11 09:35:17 -0400

    conversation with Bob

---
+++ src/Bob.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+        //Create a class named Bob with a main method for the following exercise.
+        //
+        //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+        //
+        //Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+        //He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+        //He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+        //He answers 'Whatever.' to anything else.
+        //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+        Scanner bob = new Scanner(System.in);
+        System.out.println("Would you like to speak with Bob? y/n");
+        String userInput=bob.next();
+        while(userInput.equalsIgnoreCase("y")){
+            System.out.println("What do you want to say to Bob?");
+            String userSays=bob.nextLine();
+            if (userSays.endsWith("?")){
+                System.out.println("Sure.");
+            }else if(userSays.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            }else if(userSays.isEmpty()){
+                System.out.println("Fine. Be that way.");
+            }else{
+                System.out.println("Whatever.");
+            }
+            System.out.println("Would you like to continue talking to Bob?");
+            userInput=bob.next();
+        }System.out.println("Thanks!");
+    }
+}
