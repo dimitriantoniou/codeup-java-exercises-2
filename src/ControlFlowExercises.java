3e2e02c397d8659c6fb5fafef313e6a975f8c4c6
Dimitri <dma2012@gmail.com>
2020-10-11 07:59:14 -0400

    while, do while, and for loops

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,51 @@
+public class ControlFlowExercises {
+    public static void main (String[] args){
+        //Loop Basics
+        //
+        //While
+        //
+        //Create an integer variable i with a value of 5.
+        //Create a while loop that runs so long as i is less than or equal to 15
+        //Each loop iteration, output the current value of i, then increment i by one.
+        //Your output should look like this:
+        //
+        //5 6 7 8 9 10 11 12 13 14 15
+
+        int i=5;
+        while (i<=15){
+            System.out.print(i+" ");
+            i++;
+        }
+
+        //Do While
+        //
+        //Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        int a=0;
+        do{
+            System.out.println(a);
+            a+=2;
+        }while(a<=100);
+        for(int y=0;y<+100;y+=2){
+            System.out.println(y);
+        }
+        //Alter your loop to count backwards by 5's from 100 to -10.
+        int b=100;
+        do{
+            System.out.println(b);
+            b-=2;
+        }while(b>=0);
+        for(int z=100;z>=0;z-=2){
+            System.out.println(z);
+        }
+        //Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        int x=2;
+        do{
+            System.out.println(x);
+            x= (int) Math.pow(x,2);
+        }while(x<1000000);
+        for (int j=2;j<=100000;j=(int)Math.pow(j,2)){
+            System.out.println(j);
+        }
+
+    }
+}
