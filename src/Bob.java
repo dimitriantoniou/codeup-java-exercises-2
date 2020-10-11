ac637262b220e86fd44632023df7ec67a45b0f18
Dimitri <dma2012@gmail.com>
2020-10-11 09:41:10 -0400

    fixed bob bug

--- src/Bob.java
+++ src/Bob.java
@@ -13,21 +13,24 @@ public class Bob {
         //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
         Scanner bob = new Scanner(System.in);
         System.out.println("Would you like to speak with Bob? y/n");
-        String userInput=bob.next();
-        while(userInput.equalsIgnoreCase("y")){
-            System.out.println("What do you want to say to Bob?");
-            String userSays=bob.nextLine();
-            if (userSays.endsWith("?")){
-                System.out.println("Sure.");
-            }else if(userSays.endsWith("!")){
-                System.out.println("Whoa, chill out!");
-            }else if(userSays.isEmpty()){
-                System.out.println("Fine. Be that way.");
-            }else{
-                System.out.println("Whatever.");
-            }
-            System.out.println("Would you like to continue talking to Bob?");
-            userInput=bob.next();
+        String userInput=bob.next(); //userInput is y or n
+        if(userInput.equalsIgnoreCase("y")){//if the user inputs y to keep talking
+            do {
+                System.out.println("What do you want to say to Bob?");
+                String userSays = bob.next();
+
+                if (userSays.endsWith("?")) {
+                    System.out.println("Sure.");
+                } else if (userSays.endsWith("!")) {
+                    System.out.println("Whoa, chill out!");
+                } else if (userSays.equals("")) {
+                    System.out.println("Fine. Be that way.");
+                } else {
+                    System.out.println("Whatever.");
+                }
+                System.out.println("Would you like to continue talking to Bob?");
+                userInput = bob.next();
+            }while(userInput.equalsIgnoreCase("y"));
         }System.out.println("Thanks!");
     }
 }
