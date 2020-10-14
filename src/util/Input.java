def1d5f2247d9f633f6b3cf8e861fb83e4187381
Dimitri <dma2012@gmail.com>
2020-10-14 06:55:22 -0400

    added methods to InputTest

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,19 +3,19 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner;
+    private static Scanner scanner = new Scanner(System.in);
     //String getString()
-    public String getString(){
+    public static String getString(){
         return scanner.nextLine();
     }
     //boolean yesNo()
-    public boolean yesNo(){
+    public static boolean yesNo(){
         if(scanner.next().toLowerCase().contains("y")){
             return true;
         }return false;
     }
     //int getInt(int min, int max)
-    public int getInt(int min, int max){
+    public static int getInt(int min, int max){
         int number = scanner.nextInt();
         if(number<min||number>max){
             System.out.println("Please try again.");
@@ -23,11 +23,11 @@ public class Input {
         }return number;
     }
     //int getInt()
-    public int getINt(){
+    public static int getINt(){
         return scanner.nextInt();
     }
     //double getDouble(double min, double max)
-    public double getDouble(double min, double max){
+    public static double getDouble(double min, double max){
         double number = scanner.nextDouble();
         if(number<min||number>max){
             System.out.println("Please try again.");
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,10 +3,15 @@ package util;
 public class InputTest {
     public static void main(String[] args){
         //String getString()
+        Input.getString();
         //boolean yesNo()
+        Input.yesNo();
         //int getInt(int min, int max)
+        Input.getInt(1,10);
         //int getInt()
+        //Input.getInt();
         //double getDouble(double min, double max)
+        Input.getDouble(0,10);
         //double getDouble()
     }
 }
