e1cde274040090f10f28d878bfcde8ed7f1c1240
Dimitri <dma2012@gmail.com>
2020-10-14 07:09:03 -0400

    imported util.Input and added print statements for area and circumference

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,13 +1,17 @@
 package shapes;
 
+import util.Input;
+
 public class Circle {
     //This class should have a private radius property that is set through the constructor and
     //public Circle(double radius)
     //public double getArea()
     //public double getCircumference()
     private double radius;
-    public Circle(double radius){
-        this.radius=radius;
+    public Circle(){
+        radius= Input.getDouble();
+        System.out.println("The area is: "+getArea());
+        System.out.println("The circumference is: "+getCircumference());
     }
 
     public double getArea(){
