f7e9b41b3fc2f915a6957135cc178f92cb09fc15
Dimitri <dma2012@gmail.com>
2020-10-14 06:39:58 -0400

    created InputTest class

---
+++ src/util/InputTest.java
@@ -0,0 +1,12 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args){
+        //String getString()
+        //boolean yesNo()
+        //int getInt(int min, int max)
+        //int getInt()
+        //double getDouble(double min, double max)
+        //double getDouble()
+    }
+}
