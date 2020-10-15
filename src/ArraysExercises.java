7ba55eac798b9a2442ea0f910470cee1ef88e13b
Dimitri <dma2012@gmail.com>
2020-10-15 08:09:42 -0400

    working on print formatting bug

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,15 +1,23 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
+
     public static void main (String[] args){
+
+        //int[] numbers = {1, 2, 3, 4, 5};
+        //System.out.println(Arrays.toString(numbers));
+
         //Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element. Iterate through the array and print out the name of each person in the array.
         Person[] people = new Person[3];
         Person dimitri = new Person("Dimitri");
         Person eleanna = new Person("Eleanna");
         Person mike = new Person("Mike");
 
-        people[0]= dimitri;
-        people[1]= eleanna;
-        people[2]= mike;
-        System.out.println(people);
+        people = {dimitri, eleanna, mike};
+        //people[0]= dimitri;
+        //people[1]= eleanna;
+        //people[2]= mike;
+        System.out.println(Arrays.toString(people));
 
         for (Person person:people){
             System.out.println(person);
@@ -20,5 +28,6 @@ public class ArraysExercises {
         // as well as a single person object to add to the passed array.
         // It should return an array whose length is 1 greater than the passed array,
         // with the passed person object at the end of the array.
+
     }
 }
