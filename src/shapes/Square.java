f17fc4a9cb5c23ca832b3b7cfa9cd049f9476daf
Dimitri <dma2012@gmail.com>
2020-10-16 07:13:13 -0400

    override square getArea and getPerimeter methods

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,5 +5,18 @@ public class Square extends Rectangle{
         super(side,side);
         this.length=side;
         this.width=side;
+
+        //In the Square class, override the getArea and getPerimeter methods
+        // with the following definitions for a square
+        //perimeter = 4 x side
+        //area = side ^ 2
+        public double getArea(){
+            double area =  side*side;
+            System.out.println("The area is: "+area);
+        }
+        public double getPerimeter(){
+            double perimeter = 4*side;
+            System.out.println("The perimeter is: "+perimeter);
+        }
     }
 }
