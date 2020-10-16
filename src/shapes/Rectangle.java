b5fad068b0527c3ff63bb92bb7bac6dafe0b12a0
Dimitri <dma2012@gmail.com>
2020-10-16 06:56:20 -0400

    created Rectangle class with protected propertieis and public constructor

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Rectangle {
+    //It should have protected properties for both length and width.
+    // Rectangle should define a constructor that accepts two numbers for length and width,
+    // and sets those properties.
+    protected int length;
+    protected int width;
+
+    public Rectangle(int length, int width){
+        this.length=length;
+        this.width=width.
+    }
+
+    //Create two methods on the Rectangle class, getArea and getPerimeter
+    // that return the respective values. The formulas for both follow:
+}
