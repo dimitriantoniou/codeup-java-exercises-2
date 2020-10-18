05faf201b413411f239c08a1fbcd43beb1357239
Dimitri <dma2012@gmail.com>
2020-10-18 08:47:24 -0400

    created Student class with public constructor

---
+++ src/shapes/Student.java
@@ -0,0 +1,13 @@
+package shapes;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+    public Student (String name){
+        this.name=name;
+        this.grades= new ArrayList<>();
+
+    }
+}
