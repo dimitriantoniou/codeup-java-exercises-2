b08bdc306b31335700db54a3d4cd1d9e109902ad
Dimitri <dma2012@gmail.com>
2020-10-18 08:51:29 -0400

    added addGrade, getName, and getGradeAverage methods

--- src/shapes/Student.java
+++ src/shapes/Student.java
@@ -8,6 +8,23 @@ public class Student {
     public Student (String name){
         this.name=name;
         this.grades= new ArrayList<>();
+    }
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
 
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        int sum =0;
+        for(int i=0;i<grades.size();i++){
+            sum +=grades.get(i);
+        }
+        int average = sum/grades.size();
+        return average;
     }
 }
