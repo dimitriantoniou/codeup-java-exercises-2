4d7647d604d1d8b84f35a7beb161a2737b7159ac
Dimitri <dma2012@gmail.com>
2020-10-19 19:21:08 -0400

    working on return type error

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,28 +1,21 @@
 package shapes;
 
 public class Rectangle extends Quadrilateral implements Measurable {
-    public Rectangle(double length, double width) {
-        super(length, width);
+    protected double length;//protected property for length
+    protected double width;//protected property for width
+
+    public Rectangle(double length, double width){//define a constructor that accepts 2 numbers for length and width, set those properties
+        super(length,width);
     }
 
-    @Override
-    public void setLength() {
-
+    public double getArea(){    //create a getArea method
+        return length*width;
     }
 
-    @Override
-    public void setWidth() {
+    public double getPerimeter(){    //create a getPerimeter method
+        return 2*(length+width);
     }
-
-    @Override
-    public double getPerimeter() {
-        return 0;
-    }
-
-    @Override
-    public double getArea() {
-        return 0;
-    }
+}
 
     /* inheritance and polymorphism code
     //It should have protected properties for both length and width.
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,20 +3,16 @@ package shapes;
 public class Square extends Rectangle{
     public Square (double side){
         super(side,side);
-        this.length=side;
-        this.width=side;
 
         //In the Square class, override the getArea and getPerimeter methods
         // with the following definitions for a square
         //perimeter = 4 x side
         //area = side ^ 2
         public double getArea(){
-            double area =  side*side;
-            System.out.println("The area is: "+area);
+            return super.width*super.width;
         }
         public double getPerimeter(){
-            double perimeter = 4*side;
-            System.out.println("The perimeter is: "+perimeter);
+            return 4*super.width;
         }
     }
 }
